f0d5594ab3ab854d6c11bc2fcf34619341c4458d
allyyy-za <56779689+allyyy-za@users.noreply.github.com>
2022-03-23 22:14:28 +0800

    modified websiteAPA.js

--- utils/apa/citing/websiteAPA.js
+++ utils/apa/citing/websiteAPA.js
@@ -1,9 +1,5 @@
 const { getFormattedAuthors, getFormattedLink } = require("../helper/format");
 
-// Lastname, A. (year). Title of the website
-// article in sentence case. Name of the 
-// Website in Title Case. www.website.com
-
 const citeWebsite = ({ authors, year, article, website }) => {
   const formattedAuthors = getFormattedAuthors(authors);
   const formattedYear = year ? year : "n.d.";
